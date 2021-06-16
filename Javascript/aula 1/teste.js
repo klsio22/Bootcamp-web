ed663baae4578f42d4d58c5cca20edd6f611cbb2
klesio25 <klesio.k9@gmail.com>
2021-06-16 08:33:28 -0300

    add testes js

---
+++ Javascript/aula 1/teste.js
@@ -0,0 +1,35 @@
+/* let nome = "Klesio";
+let idade = parseInt("10");
+let idade2 = parseInt("14");
+let resutado = idade + idade2;
+let nomeM = nome.toLocaleUpperCase();
+
+console.log("O " + nomeM + " tem " + resutado + " anos de idade"); */
+
+
+let nomes = ["klesio" , "Paulo"]
+
+
+nomes.pop()//remove ultimo elemento
+nomes.push(10)//Adiciona elemento
+
+console.log(nomes.toString())
+
+console.log(nomes.toString()[0])
+
+console.log(nomes.join(" - "))
+
+// dicionarios
+
+
+console.log("Dicionario\n")
+
+let frutas = [{nome:"maças",cor:"vermelhas"},{nome:"uva",cor:"roxa"}]
+
+
+console.log(frutas[1].nome , frutas[1].cor)
+
+
+
+
+
