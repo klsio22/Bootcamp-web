8c1ee86c4bda6970779a49693ff15754363e8ef2
klesio25 <klesio.k9@gmail.com>
2021-07-01 22:15:46 -0300

    create snake motion

--- snaker/js/script.js
+++ snaker/js/script.js
@@ -9,6 +9,8 @@ snake[0] = {
   y: 8 * box,
 };
 
+let direction = "right";
+
 function criarBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -21,5 +23,26 @@ function criarSnake() {
   }
 }
 
-criarBG();
-criarSnake();
+function iniciarJogo() {
+  criarBG();
+  criarSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snake -= box;
+  if (direction == "up") snake -= box;
+  if (direction == "down") snake += box;
+
+  snake.pop();
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+  
+}
+
+let jogo = setInterval(iniciarJogo, 100);
