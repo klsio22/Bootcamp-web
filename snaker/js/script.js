f57b39e1f981bc5c44ecf435ff543f021f6daded
klesio25 <klesio.k9@gmail.com>
2021-07-03 15:51:43 -0300

    Create food snake

--- snaker/js/script.js
+++ snaker/js/script.js
@@ -1,7 +1,6 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-
 let snake = [];
 
 snake[0] = {
@@ -11,16 +10,26 @@ snake[0] = {
 
 let direction = "right";
 
-function criarBG() {
+function createBackgraund() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarSnake() {
+function createSnake() {
   for (i = 0; i < snake.length; i++) {
     context.fillStyle = "#242424";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+};
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -32,14 +41,15 @@ function update(event) {
   if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-function iniciarJogo() {
+function playGame() {
   if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
   if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
-  criarBG();
-  criarSnake();
+  createBackgraund();
+  createSnake();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -59,4 +69,4 @@ function iniciarJogo() {
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(playGame, 100);
