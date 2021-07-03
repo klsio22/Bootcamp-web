1248b05e9caca54f3635ddcf3f72e815e3cb4419
klesio25 <klesio.k9@gmail.com>
2021-07-03 19:05:06 -0300

    add collision snake

--- snaker/js/script.js
+++ snaker/js/script.js
@@ -34,6 +34,7 @@ function drawFood() {
 
 document.addEventListener("keydown", update);
 
+
 function update(event) {
   if (event.keyCode == 37 && direction != "right") direction = "left";
   if (event.keyCode == 38 && direction != "down") direction = "up";
@@ -41,32 +42,54 @@ function update(event) {
   if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
+function collision() {
+  
+    
+  
+}
+
 function playGame() {
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
   if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
   if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  
+  for (i = 0; i < snake.length; i++) {
 
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearTimeout(game);
+      alert("game over");
+    }
+
+  }
+  
   createBackgraund();
   createSnake();
   drawFood();
 
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
 
   if (direction == "right") snakeX += box;
   if (direction == "left") snakeX -= box;
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
 
-  snake.pop();
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
 
-  let newHead = {
-    x: snakeX,
-    y: snakeY,
-  };
 
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(playGame, 100);
+var game = setInterval(playGame, 100);
