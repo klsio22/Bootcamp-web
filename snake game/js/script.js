2bac67e6179a8240a9fb95c15c289338fd1b6782
klesio25 <klesio.k9@gmail.com>
2021-10-31 14:47:30 -0300

    Refactored snake code

--- snake game/js/script.js
+++ snake game/js/script.js
@@ -1,47 +1,45 @@
 let canvas = document.getElementById("snake"); //id do canvas
 let context = canvas.getContext("2d");/* O metodo HTMLCanvasElement.getContext() retorna um contexto de desenho no canvas */
 
-let box = 32; //Tamanho da caixa
+const BOX = 32; //Tamanho da caixa
 let snake = [];
 
 snake[0] = {
-  x: 8 * box, //Tamanho da snake no eixo x
-  y: 8 * box, //Tamanho da snake no eixo y
+  x: 8 * BOX, //Tamanho da snake no eixo x
+  y: 8 * BOX, //Tamanho da snake no eixo y
 };
 
-let direction = "right";
+let direction = "right";//Definir um direção inicial da snake
 
 function createBackgraund() {
   context.fillStyle = "lightgreen"; //Definir a cor da caixa
-  /*  */
-
-  context.fillRect(0, 0, 16 * box, 16 * box);
+  context.fillRect(0, 0, 16 * BOX, 16 * BOX);
 
   /* O método CanvasRenderingContext2D.fillRect() da API Canvas 2D desenha um retângulo preenchido na posição (x, y)   
   void ctx.fillRect(x, y, width, height);
   */
 }
+
 //Criar pixel da snake
 function createSnake() {
   for (i = 0; i < snake.length; i++) {
     context.fillStyle = "#242424";
-    context.fillRect(snake[i].x, snake[i].y, box, box);
+    context.fillRect(snake[i].x, snake[i].y, BOX, BOX);
   }
 }
 
 let food = {
-  x: Math.floor(Math.random() * 15 + 1) * box, //Posição aleatoria da comida no eixo x
-  y: Math.floor(Math.random() * 15 + 1) * box, // Posição aleatoria da comida no eixo y
+  x: Math.floor(Math.random() * 15 + 1) * BOX, //Posição aleatoria da comida no eixo x
+  y: Math.floor(Math.random() * 15 + 1) * BOX, // Posição aleatoria da comida no eixo y
 };
 
 //Desenha a comida da snake
 function drawFood() {
   context.fillStyle = "red";
-  context.fillRect(food.x, food.y, box, box);
+  context.fillRect(food.x, food.y, BOX, BOX);
 }
 
 //alvo.addEventListener(type,listener[, options]);
-
 document.addEventListener("keydown", update);
 
 //https://keycode.info/ site que identifica as teclas do teclado
@@ -65,10 +63,10 @@ function collision() {
 
 function playGame() {
   // Limites da caixa para atravesar as paredes
-  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  if (snake[0].x > 15 * BOX && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * BOX;
+  if (snake[0].y > 15 * BOX && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * BOX;
 
   createBackgraund();
   createSnake();
@@ -78,17 +76,17 @@ function playGame() {
   let snakeX = snake[0].x; //saber a posição da snake no eixo x
   let snakeY = snake[0].y; //saber a posição da snake no eixo y
 
-  if (direction == "right") snakeX += box;
-  if (direction == "left") snakeX -= box;
-  if (direction == "up") snakeY -= box;
-  if (direction == "down") snakeY += box;
+  if (direction == "right") snakeX += BOX;
+  if (direction == "left") snakeX -= BOX;
+  if (direction == "up") snakeY -= BOX;
+  if (direction == "down") snakeY += BOX;
 
   //Define a posição aleatoria da comida dentro da caixa aleatoriamente
   if (snakeX != food.x || snakeY != food.y) {
     snake.pop();
   } else {
-    food.x = Math.floor(Math.random() * 15 + 1) * box;
-    food.y = Math.floor(Math.random() * 15 + 1) * box;
+    food.x = Math.floor(Math.random() * 15 + 1) * BOX;
+    food.y = Math.floor(Math.random() * 15 + 1) * BOX;
   }
 
   //Define a cabeça da snake
@@ -103,4 +101,4 @@ function playGame() {
 }
 
 //Executa a o game a cada 100 milissegundos
-var game = setInterval(playGame, 100);
+let game = setInterval(playGame, 100);
